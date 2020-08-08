3be042250eeea8e2c2ea04b3e0d630dd36f0fc3c
Maximiliano <maxponce.marquez@gmail.com>
2020-08-08 10:21:26 -0700

    Components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContentComponent } from './components/content/content.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ContentComponent,
+    CardsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  public title: string;
+  public width: any;
+
+  constructor() {
+    this.title = 'Mecatronica from Scratch';
+  }
+
+  ngOnInit(): void {
+    this.updateTitle();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.updateTitle();
+  }
+
+  private updateTitle() {
+    this.width = window.innerWidth;
+    const OPTION = this.getTitle( this.width );
+    switch (OPTION) {
+      case 0:
+        this.title = "M. Scratch";
+        break;
+      case 1:
+        this.title = "M. from Scratch";
+        break;
+      default:
+        this.title = "Mecatronica from Scratch";
+        break;
+    }
+  }
+
+  private getTitle( width: number ): number {
+    if ( width <= 308 ) {
+      return 0;
+    } else if ( width > 308 && width <= 474  ) {
+      return 1;
+    } else {
+      return 2;
+    }
+  }
+
+}
