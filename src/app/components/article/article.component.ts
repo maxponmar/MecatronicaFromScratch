ff22c0cfb95d5768090256d22e9ad9c0e5c3e009
Maximiliano Ponce Marquez <maxponce.marquez@gmail.com>
2020-09-21 11:24:30 -0700

    Setting up to production

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -6,47 +6,53 @@ import { DatabaseService } from 'src/app/services/database.service';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
-
   article: Article;
   sections = document.querySelectorAll('section[id]');
 
   constructor(
     private route: ActivatedRoute,
     private databaseService: DatabaseService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.article = this.databaseService.getArticleByLink(
-      this.route.snapshot.params.category,
-      this.route.snapshot.params.article);
+    this.route.paramMap.subscribe((params) => {
+      this.article = this.databaseService.getArticleByLink(
+        params.get('category'),
+        params.get('article')
+      );
+    });
   }
 
   @HostListener('window:scroll', ['$event'])
-  scrollActive(event){
+  scrollActive(event) {
     const scrollY = window.pageYOffset;
     this.sections.forEach((current: any) => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        const sectionId = current.getAttribute('id');
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.sidebar a[href*='+ sectionId + ']').classList.add('active');
-        } else {
-            document.querySelector('.sidebar a[href*='+ sectionId + ']').classList.remove('active');
-        }
+      const sectionHeight = current.offsetHeight;
+      const sectionTop = current.offsetTop - 50;
+      const sectionId = current.getAttribute('id');
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        document
+          .querySelector('.sidebar a[href*=' + sectionId + ']')
+          .classList.add('active');
+      } else {
+        document
+          .querySelector('.sidebar a[href*=' + sectionId + ']')
+          .classList.remove('active');
+      }
     });
   }
 
   openNav() {
-    document.getElementById("mySidebar").style.width = "200px";
-    document.getElementById("main").style.marginLeft = "200px";
+    document.getElementById('mySidebar').style.width = '200px';
+    document.getElementById('main').style.marginLeft = '200px';
   }
 
   closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById('mySidebar').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
   }
 
   scroll(id: string) {
